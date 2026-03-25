ac7f0c3339143ea9aa605d32126ea409fcc6f291
Swati Sharma <swatisharma83915@gmail.com>
2026-03-25 21:08:50 +0530

    Update emg_simulation.py

--- emg_simulation.py
+++ emg_simulation.py
@@ -1,1 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
 
+# simulate EMG signal
+time = np.linspace(0, 1, 100)
+signal = np.sin(10 * time) + np.random.normal(0, 0.5, 100)
+
+plt.plot(signal)
+plt.title("Simulated EMG Signal")
+plt.show()
