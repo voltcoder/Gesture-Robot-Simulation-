b73c8652e9ece923d67c106af6f1b5c2832c5f1a
Swati Sharma <swatisharma83915@gmail.com>
2026-03-25 21:07:35 +0530

    Update gesture_control.py

--- gesture_control.py
+++ gesture_control.py
@@ -1,1 +1,8 @@
+threshold = 0.5
 
+if np.mean(filtered_signal) > threshold:
+    gesture = "Strong Flex (Close Hand)"
+else:
+    gesture = "Relax (Open Hand)"
+
+print("Detected Gesture:", gesture)
