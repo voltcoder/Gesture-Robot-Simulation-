5a851527858cbf87d714fdbdf8dd89320ff2eee3
Swati Sharma <swatisharma83915@gmail.com>
2026-03-25 21:04:48 +0530

    Update filter.py

--- filter.py
+++ filter.py
@@ -1,1 +1,8 @@
+def moving_average(signal, window_size=5):
+    return np.convolve(signal, np.ones(window_size)/window_size, mode='valid')
 
+filtered_signal = moving_average(signal)
+
+plt.plot(filtered_signal)
+plt.title("Filtered Signal")
+plt.show()
